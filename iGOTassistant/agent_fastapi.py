1dd7222c3f986984a3b51b5fc281b3600e1e4270
Vinay Dabade <vinay.dabade@tarento.com>
2025-06-25 19:31:41 +0530

    [MOD] removed external app validation flow.

--- iGOTassistant/agent_fastapi.py
+++ iGOTassistant/agent_fastapi.py
@@ -3,7 +3,6 @@
 Agent service implementation for the Karmayogi Bharat chatbot.
 """
 
-# import uuid
 import logging
 import os
 import sys
@@ -11,8 +10,6 @@ import time
 
 import opik
 from dotenv import load_dotenv
-# import google.auth
-# import google.generativeai as genai
 from google.adk import Agent
 from google.adk import Runner
 from google.adk.artifacts import InMemoryArtifactService
@@ -21,7 +18,6 @@ from google.adk.sessions import DatabaseSessionService
 from google.genai import types
 from opik.integrations.adk import OpikTracer
 
-# from .config.config import KB_BASE_URL, LLM_CONFIG, GOOGLE_AGENT
 from .config.config import LLM_CONFIG, GOOGLE_AGENT
 from .models.chat import Request
 from .prompt import INSTRUCTION, GLOBAL_INSTRUCTION
@@ -31,7 +27,6 @@ from .tools.cert_tools import (
     handle_certificate_qr_issues,
     handle_certificate_name_issues,
 )
-# from .tools.faq_tools import answer_general_questions
 from .tools.faq_tools import (
     answer_general_questions,
     initialize_environment,
@@ -39,46 +34,16 @@ from .tools.faq_tools import (
 )
 from .tools.otp_auth_tools import send_otp, verify_otp, check_channel
 from .tools.tools import (
-    # answer_general_questions,
     update_phone_number_tool,
 )
 from .tools.userinfo_tools import validate_user, load_details_for_registered_users, update_name
 from .tools.zoho_ticket_tools import create_support_ticket_tool
 
-# from fronend.streamlit_chat import cookie
-# from .libs.storage import GCPStorage
-# from .libs.bhashini import (DhruvaSpeechProcessor,
-#                             DhruvaTranslator,
-#                             LanguageCodes,
-#                             convert_to_wav_with_ffmpeg)
-# from .models.callbacks import before_tool
-# from .tools.userinfo_tools import validate_user
-# from .tools.tools import (
-#     # validate_user,
-#     load_details_for_registered_users,
-#     answer_general_questions,
-#     create_support_ticket_tool,
-#     handle_certificate_issues,
-#     verify_otp,
-#     send_otp,
-#     update_phone_number_tool,
-#     list_pending_contents
-# )
-
-# from uvicorn.config import LOGGING_CONFIG
-# import logging.config
 
 import coloredlogs
 
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format="%(asctime)s : %(filename)s : %(funcName)s : %(levelname)s : %(message)s",
-#     datefmt="%Y-%m-%d %H:%M:%S",
-# )
 
-# logging.config.dictConfig(LOGGING_CONFIG)
-
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("iGOT")
 level_style = dict(
     coloredlogs.DEFAULT_FIELD_STYLES
 )
@@ -88,11 +53,8 @@ coloredlogs.install(
     level='INFO',
     fmt="%(asctime)s : %(filename)s : %(funcName)s : %(levelname)s : %(message)s")
 
-logging.info("-"*100)
+logger.info("-"*100)
 
-# speech_processor = DhruvaSpeechProcessor()
-# translator = DhruvaTranslator()
-# storage = GCPStorage()
 
 opik.configure(url=os.getenv("OPIK_URL"), use_local=True)
 opik_tracer = OpikTracer(project_name=os.getenv("OPIK_PROJECT"))
@@ -105,15 +67,12 @@ def initialize_env():
         KB_AUTH_TOKEN = os.getenv('KB_AUTH_TOKEN')
         KB_DIR = initialize_knowledge_base()
         response = answer_general_questions("What is karma points?")
-        logging.info(response)
-        logging.info("Knowledge base initialized successfully.")
+        logger.info(response)
     except (ValueError, FileNotFoundError, ImportError, RuntimeError) as e:
-        logging.info("Error initializing knowledge base: %s", e)
+        logger.info("Error initializing knowledge base: %s", e)
         sys.exit(1)
 
-    logging.info("✅ Successfully initialized tools and knowledge base")
-
-initialize_env()
+    logger.info("✅ Successfully initialized tools and knowledge base")
 
 
 class ChatAgent:
@@ -133,7 +92,7 @@ class ChatAgent:
         self.chat_sessions = {}
 
     
-        logging.info("Initializing Google ADK agent")
+        logger.info("Initializing Google ADK agent")
         self.agent = Agent(
             model=os.getenv("GEMINI_MODEL"),
             name="iGOTAssistant",
@@ -170,7 +129,7 @@ class ChatAgent:
 
     async def start_new_session(self, user_id, request: Request) -> dict:
         """Start a new chat session with initial instructions."""
-        logging.info(f'{user_id} :: Trying to start new session {request.session_id}')
+        logger.info(f'{user_id} :: Trying to start new session {request.session_id}')
         if not GOOGLE_AGENT:
             chat = self.agent.start_chat(
                 history=[{
@@ -179,27 +138,23 @@ class ChatAgent:
                 }],
                 enable_automatic_function_calling=True
             )
-            # session_id = request.channel_id + "_" + request.session_id
             session_id = request.session_id
             self.chat_sessions[session_id] = {
                 "chat": chat,
                 "history": []
             }
-            logging.info(f"{user_id} :: New session started : %s", session_id)
+            logger.info(f"{user_id} :: New session started : %s", session_id)
         else:
             if await self.session_service.get_session(app_name=self.app_name, session_id=request.session_id, user_id=user_id):
                 return {"message" : "Session exist, try chat send."}
 
-            logging.info(f'{user_id} :: Session id', request.session_id)
+            logger.info(f'{user_id} :: Session id', request.session_id)
             self.user_id = user_id,
             self.session = await self.session_service.create_session(
                     app_name=self.app_name,
                     session_id=request.session_id ,
                     user_id=user_id
                     )
-            # print(self.session.app_name, self.session.session_id, self.session.__user_id)
-            # self.runner = Runner(app_name=self.app_name, agent=self.llm, session_service=self.session_service, artifact_service=self.artifact_service)
-            # print('Generated session ', self.session.session_id, self.session.user_id)
 
         return {"message": "Starting new chat session."}
 
@@ -213,10 +168,6 @@ class ChatAgent:
             parts=[types.Part.from_text(text=request.text)]
         )
 
-        # print('* user : ', content.model_dump(exclude_none=True))
-        # print('-'*40)
-        # print(request, self.session, self.user_id)
-        # session_id = request.channel_id + "_" + request.session_id
 
         if not await self.runner.session_service.get_session(app_name=self.app_name, user_id=user_id, session_id=request.session_id):
             await self.start_new_session(user_id, request)
@@ -229,7 +180,7 @@ class ChatAgent:
             )
 
             if session is not None and not session.state.get("web", False):
-                logging.info(f"{user_id} :: Setting the WEB ......")
+                logger.info(f"{user_id} :: Setting the WEB ......")
                 time_now = time.time()
                 state_changes = {
                     "web" : True,
@@ -248,26 +199,18 @@ class ChatAgent:
 
                 await self.runner.session_service.append_event(session, system_event)
 
-            logging.info(f'{user_id} :: Setting state', session.state.get("web"))
-            logging.info(f'{user_id} :: Setting user_id', session.state.get("user_id"))
+            logger.info(f'{user_id} :: Setting state', session.state.get("web"))
+            logger.info(f'{user_id} :: Setting user_id', session.state.get("user_id"))
 
-        # print(self.session.id, self.session.user_id)
         try:
-            # if not await self.runner.session_service.get_session(app_name=self.app_name, user_id=user_id, session_id=request.session_id):
-            #     await self.start_new_session(user_id, request)
-
             async for event in self.runner.run_async(
-                    # user_id=self.user_id,
                     user_id=user_id,
-                    # user_id=request.user_id,
-                    # session_id=request.session_id,
                     session_id=request.session_id,
                     new_message=content):
-                # print(event)
                 if event.content.parts and event.content.parts[0].text:
-                    logging.info(f"{user_id} :: {event.author} : {event.content.parts[0].text}")
+                    logger.info(f"{user_id} :: {event.author} : {event.content.parts[0].text}")
                     response = event.content.parts[0].text
         except Exception as e:
-            logging.info(str(e))
+            logger.info(str(e))
 
         return {"text": response, "audio": audio_url}
