4f2a56e39dd86a5096f8ee42487f1ec2eec817f4
jayaprakashnarayanaswamy <jayaprakash.n@tarento.com>
2025-10-31 12:47:08 +0530

    "Avoiding Gemini calls for start API calls"

--- main.py
+++ main.py
@@ -961,7 +961,7 @@ async def chat(
 
             try:
                 with LogExecutionTime("Agent Query Processing", "agent"):
-                    if chat_request.message.lower().strip() == "Hello":
+                    if chat_request.message == "Hello":
                         bot_response = (f"Hello {cached_user_details.first_name}! "
                                         "Welcome to Karmayogi Bharat support! I am here to assist with general platform queries and support. Please let me know how I can help you.")
                     else:
